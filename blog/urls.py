7c25fbbc08d92a3d34b25fbe9c6d40effaba26a0
Diego <diego_shakan@hotmail.com>
2019-01-19 17:55:10 -0300

    finalizando o blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import post_detail, post_list
+from . import views
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('post/<int:pk>/', post_detail, name='post_detail'),
-    
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
