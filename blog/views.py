ae9a7ddaadedb7b39a28e68c93f7089d02588f8d
Diego <diego_shakan@hotmail.com>
2019-01-20 10:24:20 -0300

    adição de api

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from rest_framework import viewsets
+from .serializers import PostModelSerializer
 
 # Create your views here.
 
@@ -12,10 +14,6 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_new(request):
      if request.method == "POST":
@@ -43,3 +41,7 @@ def post_edit(request, pk):
      else:
          form = PostForm(instance=post)
      return render(request, 'blog/post_edit.html', {'form': form})
+
+class PostModelViewSet(viewsets.ModelViewSet):
+    serializer_class = PostModelSerializer
+    queryset = Post.objects.all().order_by('-title')
